481307e7777b3a3aac3a905cf0187f223405194e
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2023-09-13 08:08:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+cy.visit(baseUrl + "/main.html");
+cy.get('h1').invoke('text').then((text) => {
+    const cleanedText = text.replace(/[\n\t]/g, ''); // Remove newline and tab characters
+    expect(cleanedText).to.equal('Welcome to Accio Jobs!');
+});
